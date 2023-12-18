27b8f25fc81b685467d88ffaedf4331944bfddfd
racoon2code <149416317+racoon2code@users.noreply.github.com>
2023-12-18 18:41:52 +0100

    Add files via upload

---
+++ Day1/Part1/AoC_Day1_Part1.py
@@ -0,0 +1,60 @@
+data = open("data.txt", "r")
+
+result = 0
+
+for row in data:
+
+    list1 = []
+    
+
+    for index, sign in enumerate(row):
+        # if sign == "e":
+        #     if index == row.find("eight", index):
+        #         list1.append(8)
+        # if sign == "f":
+        #     if index == row.find("four", index):
+        #         list1.append(4)
+        #     if index == row.find("five", index):
+        #         list1.append(5)
+        # if sign == "n":
+        #     if index == row.find("nine", index):
+        #         list1.append(9)
+        # if sign == "o":
+        #     if index == row.find("one", index):
+        #         list1.append(1)
+        # if sign == "s":
+        #     if index == row.find("seven", index):
+        #         list1.append(7)
+        #     if index == row.find("six", index):
+        #         list1.append(6)
+        # if sign == "t":
+        #     if index == row.find("three", index):
+        #         list1.append(3)
+        #     if index == row.find("two", index):
+        #         list1.append(2)
+        if sign == "1":
+            list1.append(1)
+        if sign == "2":
+            list1.append(2)
+        if sign == "3":
+            list1.append(3)
+        if sign == "4":
+            list1.append(4)       
+        if sign == "5":
+            list1.append(5)
+        if sign == "6":
+            list1.append(6)
+        if sign == "7":
+            list1.append(7)
+        if sign == "8":
+            list1.append(8)
+        if sign == "9":
+            list1.append(9)
+    
+
+    number = str(list1[0]) + str(list1[-1])
+         
+    result += int(number)
+    
+    
+print(result)
